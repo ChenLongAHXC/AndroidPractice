dfeb499c6c1d442e89be582f058194286cdfb8b9
ChenLongAHXC <chenlong.ahxc@gmail.com>
2014-04-16 23:15:34 +0800

    add ExpandableListActivity and SeekBarAndRatingBar

---
+++ ExpandableListActivity/gen/com/chenlong/expandablelistactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chenlong.expandablelistactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
