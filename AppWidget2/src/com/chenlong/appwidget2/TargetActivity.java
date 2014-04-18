59fa106c5c05e960e33f6170a05c34a7f5570a98
ChenLongAHXC <chenlong.ahxc@gmail.com>
2014-04-18 23:57:56 +0800

    add appwidget2

---
+++ AppWidget2/src/com/chenlong/appwidget2/TargetActivity.java
@@ -0,0 +1,17 @@
+package com.chenlong.appwidget2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TargetActivity extends Activity{
+
+	public TargetActivity() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.target_activity);
+	}
+}
